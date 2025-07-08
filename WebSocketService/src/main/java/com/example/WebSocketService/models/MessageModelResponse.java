78d6b8522bfa19a9370bfb0ae1017ff9fee2f6dd
Anna Zhevlakova <azevlakova12@gmail.com>
2025-07-09 01:25:30 +0400

    server

---
+++ WebSocketService/src/main/java/com/example/WebSocketService/models/MessageModelResponse.java
@@ -0,0 +1,30 @@
+package com.example.WebSocketService.models;
+
+import java.time.Instant;
+
+public class MessageModelResponse {
+    private String _login;
+    private String _message;
+    private Instant _dateTime;
+
+    public String getLogin() {
+        return _login;
+    }
+    public void setLogin(String login) {
+        _login = login;
+    }
+
+    public String getMessage() {
+        return _message;
+    }
+    public void setMessage(String message) {
+        _message = message;
+    }
+
+    public Instant getDateTime() {
+        return _dateTime;
+    }
+    public void setDateTime(Instant dateTime) {
+        _dateTime = dateTime;
+    }
+}
---
+++ WebSocketService/src/test/java/com/example/WebSocketService/WebSocketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebSocketService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
